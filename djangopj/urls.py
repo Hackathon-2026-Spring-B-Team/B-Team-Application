4f70be44a9ee3fbf91a969220f041aad0a5462d0
DN0N <hanawa.0616@gmail.com>
2026-05-27 04:20:18 +0900

    エラー修正、nginx設定変更

--- djangopj/urls.py
+++ djangopj/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("hoge/", TemplateView.as_view(template_name='hoge.html'), name="hoge"),
     path("form/", TemplateView.as_view(template_name='createplan/form.html'), name="form"),
     # path("select/", TemplateView.as_view(template_name='select.html'), name="select"),
-    path("regenerate/", TemplateView.as_view(template_name='regenerate.html'), name="regenerate"),
+    path("regenerate/", TemplateView.as_view(template_name='createplan/regenerate.html'), name="regenerate"),
     path("home/", core_views.home, name="home"),
     #path("login/", TemplateView.as_view(template_name='login.html'), name="login"),
     #path("signup/", TemplateView.as_view(template_name='signup.html'), name="signup"),
