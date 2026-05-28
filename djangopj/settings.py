23e53afc3d6a2c949cde3186b9ccdd3431d84df1
raretech-hal <halraretech@gmail.com>
2026-05-29 01:43:12 +0900

    未ログイン時のリダイレクト処理の修正

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -147,6 +147,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 認証時のリダイレクト先
-LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "signin"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "signin"
