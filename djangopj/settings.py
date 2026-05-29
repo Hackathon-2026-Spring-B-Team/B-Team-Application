23d0009ca9a239c9d62568869dc3c8bd23cd630e
DN0N <hanawa.0616@gmail.com>
2026-05-30 03:28:34 +0900

    djangoシークレットキーを.envで管理、docker-compose.ymlに開発環境用設定を記述

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)_k6q-y)r2+xxo512)hrj(+5afpn76fwt@t+beiyb_a0ml3h0%'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- dockerfiles/django/gunicorn_config.py
+++ dockerfiles/django/gunicorn_config.py
@@ -5,5 +5,5 @@ bind = '0.0.0.0:8000'
 workers = 5
 timeout = 300
 
-# プロセス設定
+# プロセス設定 マスタープロセスがアプリを先に読み込んでからworkerをコピーする
 preload_app = True
